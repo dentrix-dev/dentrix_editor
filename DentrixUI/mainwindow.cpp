058cac26573eaad5418461357699fc089e56f72e
mohamedAdhamc <mohamed.adhamnagiub@gmail.com>
2025-04-09 16:55:26 +0200

    add blue edit button to toolbar

--- DentrixUI/mainwindow.cpp
+++ DentrixUI/mainwindow.cpp
@@ -2,6 +2,8 @@
 #include "./ui_mainwindow.h"
 #include <QFileDialog>
 #include <QToolBar>
+#include <QWidgetAction>
+#include <QPushButton>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -29,11 +31,24 @@ void MainWindow::createToolBar()
     QAction *openAction = new QAction("tool 1", this);
     QAction *saveAction = new QAction("tool 2", this);
     QAction *exitAction = new QAction("tool 3", this);
+    // Add a spacer widget to push following widgets to the bottom
+    QWidget *spacer = new QWidget(this);
+    spacer->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+    QWidgetAction *spacerAction = new QWidgetAction(this);
+    spacerAction->setDefaultWidget(spacer);
+    // Create a styled blue "Edit" button
+    QPushButton *editButton = new QPushButton("Edit");
+    editButton->setStyleSheet("background-color: #3498db; color: white; padding: 5px;");
+    // Wrap it in a QWidgetAction so it can go in the QToolBar
+    QWidgetAction *editAction = new QWidgetAction(this);
+    editAction->setDefaultWidget(editButton);
 
     toolBar->addAction(openAction);
     toolBar->addAction(saveAction);
     toolBar->addSeparator(); // Adds a separator line
     toolBar->addAction(exitAction);
+    toolBar->addAction(spacerAction); // Add it before the edit button
+    toolBar->addAction(editAction);
 
     // Add toolbar to the left side
     addToolBar(Qt::LeftToolBarArea, toolBar);
