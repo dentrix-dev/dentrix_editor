ea155d68badf18223d0e04643327556f2c7312f7
MahmoudHamdy02 <mahmoud-mahgoub1@hotmail.com>
2025-06-14 23:20:27 +0300

    add readOBJ method with remap

--- DentrixCore/filehandler.h
+++ DentrixCore/filehandler.h
@@ -10,7 +10,8 @@ class FileHandler
 public:
     FileHandler();
 
-    static std::vector<Mesh*> readOBJ(std::string& filepath);
+    static pmp::SurfaceMesh readOBJ(const std::string& filepath, std::vector<pmp::Face>* remap = nullptr);
+    static std::vector<Mesh*> readSegmentedOBJ(std::string& filepath);
     static Mesh* readSTL(std::string& filepath, std::string& name);
 
     // File type is determined by the file extension
